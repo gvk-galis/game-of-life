91d8297b4fda635ffa8c8bbbff748cdd849619ae
gvk-galis <62999147+gvk-galis@users.noreply.github.com>
2020-05-17 15:17:50 +0530

    commit #

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,7 +17,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
- * new commit on github
+ * new commit on github#
  */
 @Controller
 @RequestMapping("/game")
